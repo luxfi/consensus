2fa6cdb288c7db7453a7c748514a555e7dcab611
Zach Kelling <z@zeekay.io>
2025-08-15 15:55:56 +0000

    Fix remaining type issues for Beta uint32 migration and add FPC methods to TestBlock

--- chain/chaintest/test_blocks.go
+++ chain/chaintest/test_blocks.go
@@ -89,4 +89,14 @@ func (b *TestBlock) Status() choices.Status {
 	return b.StatusV
 }
 
+// FPCVotes returns embedded fast-path vote references
+func (b *TestBlock) FPCVotes() [][]byte {
+	return nil // Test blocks don't have FPC votes
+}
+
+// EpochBit returns the epoch fence bit
+func (b *TestBlock) EpochBit() bool {
+	return false // Test blocks don't have epoch bits
+}
+
 var _ chain.Block = (*TestBlock)(nil)
