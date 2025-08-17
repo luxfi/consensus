e4892a055e8f9b45d0b5c3d7c90b1bd40c3ef487
Zach Kelling <z@zeekay.io>
2025-08-17 00:35:02 +0000

    Fix duplicate Manager interface declaration

---
+++ networking/router/health.go
@@ -0,0 +1,22 @@
+// Copyright (C) 2019-2024, Lux Industries Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package router
+
+import "time"
+
+// HealthConfig defines health check configuration for the router
+type HealthConfig struct {
+	// MaxDropRate is the maximum percentage of messages that can be dropped
+	// before the router is considered unhealthy
+	MaxDropRate float64 `json:"maxDropRate"`
+	// MaxOutstandingRequests is the maximum number of outstanding requests
+	// allowed before the router is considered unhealthy
+	MaxOutstandingRequests int `json:"maxOutstandingRequests"`
+	// MaxOutstandingDuration is the maximum time a request can be outstanding
+	// before the router is considered unhealthy
+	MaxOutstandingDuration time.Duration `json:"maxOutstandingDuration"`
+	// MaxRunTimeHealthy is the maximum time the router can run before needing
+	// a health check
+	MaxRunTimeHealthy time.Duration `json:"maxRunTimeHealthy"`
+}
--- networking/router/router.go
+++ networking/router/router.go
@@ -2,8 +2,10 @@ package router
 
 import (
 	"context"
+	"time"
 
 	"github.com/luxfi/ids"
+	"github.com/luxfi/node/version"
 )
 
 // InboundMessage represents an inbound message
@@ -17,4 +19,20 @@ type Router interface {
 	AddChain(chainID ids.ID, handler interface{})
 	RemoveChain(chainID ids.ID)
 	HandleInbound(ctx context.Context, msg InboundMessage)
+	Connected(nodeID ids.NodeID, nodeVersion *version.Application, subnetID ids.ID)
+	Disconnected(nodeID ids.NodeID)
+	AppGossip(ctx context.Context, nodeID ids.NodeID, msg []byte) error
+	Initialize(
+		nodeID ids.NodeID,
+		log interface{},
+		timeoutManager interface{},
+		closeTimeout time.Duration,
+		criticalChains interface{},
+		sybilProtectionEnabled bool,
+		trackedSubnets interface{},
+		onFatal func(int),
+		healthConfig HealthConfig,
+		metricsRegisterer interface{},
+	) error
+	HealthCheck(ctx context.Context) (interface{}, error)
 }
