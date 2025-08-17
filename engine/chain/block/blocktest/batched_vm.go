d4e83c70d33f0a9fd1f51efddaff70e9d4ff8205
Zach Kelling <z@zeekay.io>
2025-08-17 06:51:27 +0000

    Fix test compilation in blocktest and enginetest packages
    - Add VM struct implementation for block.ChainVM interface
    - Add BatchedVM for block.BatchedChainVM interface
    - Add StateSyncableVM for state sync testing
    - Remove unused database import from enginetest
    - Implement all required methods for test VMs

---
+++ engine/chain/block/blocktest/batched_vm.go
@@ -0,0 +1,84 @@
+// Copyright (C) 2019-2025, Lux Industries Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package blocktest
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/luxfi/consensus/engine/chain/block"
+	"github.com/luxfi/ids"
+)
+
+var (
+	errGetAncestor       = errors.New("unexpectedly called GetAncestor")
+	errBatchedParseBlock = errors.New("unexpectedly called BatchedParseBlock")
+
+	_ block.BatchedChainVM = (*BatchedVM)(nil)
+)
+
+// BatchedVM is a BatchedVM that is useful for testing.
+type BatchedVM struct {
+	VM
+
+	CantGetAncestors    bool
+	CantBatchParseBlock bool
+
+	GetAncestorsF func(
+		ctx context.Context,
+		blkID ids.ID,
+		maxBlocksNum int,
+		maxBlocksSize int,
+		maxBlocksRetrivalTime time.Duration,
+	) ([][]byte, error)
+
+	BatchedParseBlockF func(
+		ctx context.Context,
+		blks [][]byte,
+	) ([]block.Block, error)
+}
+
+func (vm *BatchedVM) Default(cant bool) {
+	vm.VM.Default(cant)
+	vm.CantGetAncestors = cant
+	vm.CantBatchParseBlock = cant
+}
+
+func (vm *BatchedVM) GetAncestors(
+	ctx context.Context,
+	blkID ids.ID,
+	maxBlocksNum int,
+	maxBlocksSize int,
+	maxBlocksRetrivalTime time.Duration,
+) ([][]byte, error) {
+	if vm.GetAncestorsF != nil {
+		return vm.GetAncestorsF(
+			ctx,
+			blkID,
+			maxBlocksNum,
+			maxBlocksSize,
+			maxBlocksRetrivalTime,
+		)
+	}
+	if vm.CantGetAncestors && vm.VM.T != nil {
+		require.FailNow(vm.VM.T, errGetAncestor.Error())
+	}
+	return nil, errGetAncestor
+}
+
+func (vm *BatchedVM) BatchedParseBlock(
+	ctx context.Context,
+	blks [][]byte,
+) ([]block.Block, error) {
+	if vm.BatchedParseBlockF != nil {
+		return vm.BatchedParseBlockF(ctx, blks)
+	}
+	if vm.CantBatchParseBlock && vm.VM.T != nil {
+		require.FailNow(vm.VM.T, errBatchedParseBlock.Error())
+	}
+	return nil, errBatchedParseBlock
+}
