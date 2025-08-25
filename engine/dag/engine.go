035d4ed60ed344eb6720d3ec2e849801eadad55a
Zach Kelling <z@zeekay.io>
2025-08-25 02:05:31 +0000

    Add vertex and DAG engine types for node compatibility

--- engine/dag/engine.go
+++ engine/dag/engine.go
@@ -1,59 +1,39 @@
+// Copyright (C) 2019-2024, Lux Industries Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
 package dag
 
 import (
 	"context"
+
 	"github.com/luxfi/ids"
 )
 
-// Engine defines the DAG consensus engine
+// Transaction represents a DAG transaction
+type Transaction interface {
+	ID() ids.ID
+	Parent() ids.ID
+	Height() uint64
+	Bytes() []byte
+	Verify(context.Context) error
+	Accept(context.Context) error
+	Reject(context.Context) error
+}
+
+// Engine defines the DAG consensus engine interface
 type Engine interface {
+	// GetVtx gets a vertex by ID
+	GetVtx(context.Context, ids.ID) (Transaction, error)
+
+	// BuildVtx builds a new vertex
+	BuildVtx(context.Context) (Transaction, error)
+
+	// ParseVtx parses a vertex from bytes
+	ParseVtx(context.Context, []byte) (Transaction, error)
+
 	// Start starts the engine
 	Start(context.Context, uint32) error
 
-	// Stop stops the engine
-	Stop(context.Context) error
-
-	// HealthCheck performs a health check
-	HealthCheck(context.Context) (interface{}, error)
-
-	// IsBootstrapped returns whether the DAG is bootstrapped
-	IsBootstrapped() bool
-}
-
-// Avalanche implements Avalanche DAG consensus
-type Avalanche struct {
-	bootstrapped bool
-}
-
-// New creates a new DAG consensus engine
-func New() *Avalanche {
-	return &Avalanche{
-		bootstrapped: false,
-	}
-}
-
-// Start starts the engine
-func (a *Avalanche) Start(ctx context.Context, requestID uint32) error {
-	a.bootstrapped = true
-	return nil
-}
-
-// Stop stops the engine
-func (a *Avalanche) Stop(ctx context.Context) error {
-	return nil
-}
-
-// HealthCheck performs a health check
-func (a *Avalanche) HealthCheck(ctx context.Context) (interface{}, error) {
-	return map[string]interface{}{"healthy": true}, nil
-}
-
-// IsBootstrapped returns whether the DAG is bootstrapped
-func (a *Avalanche) IsBootstrapped() bool {
-	return a.bootstrapped
-}
-
-// GetVertex gets a vertex by ID
-func (a *Avalanche) GetVertex(ctx context.Context, nodeID ids.NodeID, requestID uint32, vertexID ids.ID) error {
-	return nil
+	// Shutdown shuts down the engine
+	Shutdown(context.Context) error
 }
