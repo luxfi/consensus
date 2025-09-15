0c2d1d51e80a4c1691277ec4fbc72492bf8eaeeb
Hanzo Dev <dev@hanzo.ai>
2025-09-14 19:30:46 -0500

    fix: remove go.work file to fix CI

---
+++ src/rust/target/debug/build/lux-consensus-b4d5594ecd3f5953/build_script_build-b4d5594ecd3f5953.d
@@ -0,0 +1,5 @@
+/Users/z/work/lux/consensus/src/rust/target/debug/build/lux-consensus-b4d5594ecd3f5953/build_script_build-b4d5594ecd3f5953.d: build.rs
+
+/Users/z/work/lux/consensus/src/rust/target/debug/build/lux-consensus-b4d5594ecd3f5953/build_script_build-b4d5594ecd3f5953: build.rs
+
+build.rs:
