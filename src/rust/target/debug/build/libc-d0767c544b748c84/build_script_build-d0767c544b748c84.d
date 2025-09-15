672a5f5481d7dc718f9bc7a405f3be2dc341a7fa
Zach Kelling <z@zeekay.io>
2025-09-14 19:30:46 -0500

    fix: remove go.work file to fix CI

---
+++ src/rust/target/debug/build/libc-d0767c544b748c84/build_script_build-d0767c544b748c84.d
@@ -0,0 +1,5 @@
+/Users/z/work/lux/consensus/src/rust/target/debug/build/libc-d0767c544b748c84/build_script_build-d0767c544b748c84.d: /Users/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.175/build.rs
+
+/Users/z/work/lux/consensus/src/rust/target/debug/build/libc-d0767c544b748c84/build_script_build-d0767c544b748c84: /Users/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.175/build.rs
+
+/Users/z/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.175/build.rs:
