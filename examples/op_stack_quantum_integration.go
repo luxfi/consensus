79315d16929a739b800865a94c5d4eb1ea5e57c1
Hanzo Dev <dev@hanzo.ai>
2025-11-06 13:57:34 -0800

    fix: Update to Go 1.25.4, remove go-ethereum, fix CI lint
    - Upgrade Go version from 1.25.1 to 1.25.4 (latest stable)
    - Remove go-ethereum dependency and use luxfi/geth instead
    - Update op_stack example to import from luxfi/geth
    - Fix golangci-lint to use project Go version instead of bundled version
    - Install golangci-lint via go install to ensure correct Go version
    
    This addresses CI lint failures where golangci-lint was using Go 1.24
    instead of the required Go 1.25+.

--- examples/op_stack_quantum_integration.go
+++ examples/op_stack_quantum_integration.go
@@ -17,9 +17,9 @@ import (
 	"math/big"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/luxfi/geth/common"
+	"github.com/luxfi/geth/core/types"
+	"github.com/luxfi/geth/crypto"
 	"github.com/luxfi/consensus/engine/core"
 	"github.com/luxfi/ids"
 )
