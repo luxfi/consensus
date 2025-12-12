ad97b72276acd8af5f8f305c6c22044d6e77b3e6
Zach Kelling <z@zeekay.ai>
2025-12-12 05:19:18 -0800

    refactor: remove warp type aliases from consensus
    Warp types don't belong in consensus - consumers should import
    directly from github.com/luxfi/warp. Also flatten engine/core/common
    into engine/core to reduce unnecessary nesting.
    
    - Remove core/types.go (warp re-exports)
    - Remove engine/core/common/ (merge into engine/core)
    - Remove warp aliases from router, block/vm.go, errors.go
    - Update bft imports to use engine/core directly

--- engine/core/core.go
+++ engine/core/core.go
@@ -1,10 +1,9 @@
+// Copyright (C) 2019-2025, Lux Industries Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
 package core
 
-import (
-	"github.com/luxfi/consensus/engine/core/common"
-	"github.com/luxfi/ids"
-	"github.com/luxfi/warp"
-)
+import "github.com/luxfi/ids"
 
 // Fx represents a feature extension
 type Fx struct {
@@ -12,22 +11,91 @@ type Fx struct {
 	Fx interface{}
 }
 
-// Sender sends warp messages with RingTail post-quantum signatures.
-type Sender = warp.Sender
+// Message represents a network message
+type Message struct {
+	Type    MessageType
+	NodeID  ids.NodeID
+	Content []byte
+}
 
-// Handler handles incoming warp messages.
-type Handler = warp.Handler
+// MessageType defines the type of a message
+type MessageType uint32
 
-// Error represents a warp error.
-type Error = warp.Error
+// String returns the string representation of the message type
+func (m MessageType) String() string {
+	switch m {
+	case PendingTxs:
+		return "PendingTxs"
+	case PutBlock:
+		return "PutBlock"
+	case GetBlock:
+		return "GetBlock"
+	case GetAccepted:
+		return "GetAccepted"
+	case Accepted:
+		return "Accepted"
+	case GetAncestors:
+		return "GetAncestors"
+	case MultiPut:
+		return "MultiPut"
+	case GetFailed:
+		return "GetFailed"
+	case QueryFailed:
+		return "QueryFailed"
+	case Chits:
+		return "Chits"
+	case ChitsV2:
+		return "ChitsV2"
+	case GetAcceptedFrontier:
+		return "GetAcceptedFrontier"
+	case AcceptedFrontier:
+		return "AcceptedFrontier"
+	case GetAcceptedFrontierFailed:
+		return "GetAcceptedFrontierFailed"
+	case WarpRequest:
+		return "WarpRequest"
+	case WarpResponse:
+		return "WarpResponse"
+	case WarpGossip:
+		return "WarpGossip"
+	default:
+		return "Unknown"
+	}
+}
 
-// SendConfig configures message sending parameters.
-type SendConfig = warp.SendConfig
-
-// MessageType represents the type of message
-type MessageType = common.MessageType
-
-// Message constants
 const (
-	PendingTxs = common.PendingTxs
+	// PendingTxs indicates pending transactions
+	PendingTxs MessageType = iota
+	// PutBlock indicates a block to be added
+	PutBlock
+	// GetBlock indicates a request for a block
+	GetBlock
+	// GetAccepted indicates a request for accepted blocks
+	GetAccepted
+	// Accepted indicates an accepted block
+	Accepted
+	// GetAncestors indicates a request for ancestors
+	GetAncestors
+	// MultiPut indicates multiple blocks
+	MultiPut
+	// GetFailed indicates a failed get request
+	GetFailed
+	// QueryFailed indicates a failed query
+	QueryFailed
+	// Chits indicates chits message
+	Chits
+	// ChitsV2 indicates chits v2 message
+	ChitsV2
+	// GetAcceptedFrontier indicates a request for accepted frontier
+	GetAcceptedFrontier
+	// AcceptedFrontier indicates accepted frontier
+	AcceptedFrontier
+	// GetAcceptedFrontierFailed indicates a failed frontier request
+	GetAcceptedFrontierFailed
+	// WarpRequest indicates a warp request
+	WarpRequest
+	// WarpResponse indicates a warp response
+	WarpResponse
+	// WarpGossip indicates warp gossip
+	WarpGossip
 )
