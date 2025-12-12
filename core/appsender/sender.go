e7a82ccea2d2ee9aba5a2ea8d9c69f7c546a6fa3
Zach Kelling <z@zeekay.io>
2025-12-11 22:46:11 -0800

    refactor: simplify appsender to alias warp.Sender

--- core/appsender/sender.go
+++ core/appsender/sender.go
@@ -2,23 +2,15 @@
 // See the file LICENSE for licensing terms.
 
 // Package appsender provides the AppSender interface for cross-chain communication.
+// Deprecated: Use github.com/luxfi/warp instead.
 package appsender
 
 import (
-	"context"
-
-	"github.com/luxfi/ids"
-	"github.com/luxfi/math/set"
+	"github.com/luxfi/warp"
 )
 
-// AppSender sends application-level messages to other nodes.
-type AppSender interface {
-	// SendAppRequest sends an application-level request to the given nodes.
-	SendAppRequest(ctx context.Context, nodeIDs set.Set[ids.NodeID], requestID uint32, appRequestBytes []byte) error
-	// SendAppResponse sends an application-level response to the given node.
-	SendAppResponse(ctx context.Context, nodeID ids.NodeID, requestID uint32, appResponseBytes []byte) error
-	// SendAppGossip sends an application-level gossip message to the given nodes.
-	SendAppGossip(ctx context.Context, nodeIDs set.Set[ids.NodeID], appGossipBytes []byte) error
-	// SendAppError sends an application error to the given node.
-	SendAppError(ctx context.Context, nodeID ids.NodeID, requestID uint32, errorCode int32, errorMessage string) error
-}
+// Sender is the warp Sender interface.
+type Sender = warp.Sender
+
+// Deprecated: Use Sender instead.
+type AppSender = warp.Sender
