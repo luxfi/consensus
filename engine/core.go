e7ece5c6a555b1cd32b21473ad36aea3ac3f5255
Zach Kelling <z@zeekay.io>
2025-12-13 07:45:18 +0000

    style: fix gofmt formatting issues

--- engine/core.go
+++ engine/core.go
@@ -16,22 +16,22 @@ type (
 
 // Re-export message type constants
 const (
-	PendingTxs             = core.PendingTxs
-	PutBlock               = core.PutBlock
-	GetBlock               = core.GetBlock
-	GetAccepted            = core.GetAccepted
-	Accepted               = core.Accepted
-	GetAncestors           = core.GetAncestors
-	MultiPut               = core.MultiPut
-	GetFailed              = core.GetFailed
-	QueryFailed            = core.QueryFailed
-	Chits                  = core.Chits
-	ChitsV2                = core.ChitsV2
-	GetAcceptedFrontier    = core.GetAcceptedFrontier
-	AcceptedFrontier       = core.AcceptedFrontier
+	PendingTxs                = core.PendingTxs
+	PutBlock                  = core.PutBlock
+	GetBlock                  = core.GetBlock
+	GetAccepted               = core.GetAccepted
+	Accepted                  = core.Accepted
+	GetAncestors              = core.GetAncestors
+	MultiPut                  = core.MultiPut
+	GetFailed                 = core.GetFailed
+	QueryFailed               = core.QueryFailed
+	Chits                     = core.Chits
+	ChitsV2                   = core.ChitsV2
+	GetAcceptedFrontier       = core.GetAcceptedFrontier
+	AcceptedFrontier          = core.AcceptedFrontier
 	GetAcceptedFrontierFailed = core.GetAcceptedFrontierFailed
-	WarpRequest            = core.WarpRequest
-	WarpResponse           = core.WarpResponse
-	WarpGossip             = core.WarpGossip
-	StateSyncDone          = core.StateSyncDone
+	WarpRequest               = core.WarpRequest
+	WarpResponse              = core.WarpResponse
+	WarpGossip                = core.WarpGossip
+	StateSyncDone             = core.StateSyncDone
 )
