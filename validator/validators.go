524f7cd5a05b5236a2faeb16bb14110fda78827e
Zach Kelling <z@zeekay.io>
2026-01-26 08:04:48 -0800

    chore: update dependencies and refactor validator module

--- validator/validators.go
+++ validator/validators.go
@@ -1,141 +1,41 @@
+// Package validators provides validator state management.
+// This package re-exports github.com/luxfi/validators for backward compatibility.
+// New code should use github.com/luxfi/validators directly.
 package validators
 
-//go:generate go run go.uber.org/mock/mockgen -package=validatorsmock -destination=validatorsmock/state.go -mock_names=State=State . State
-
 import (
-	"context"
-
-	"github.com/luxfi/consensus/version"
-	"github.com/luxfi/ids"
-	"github.com/luxfi/math/set"
+	"github.com/luxfi/validators"
 )
 
-// State provides validator state management
-type State interface {
-	// GetValidatorSet returns validators at a specific height for a network
-	GetValidatorSet(ctx context.Context, height uint64, netID ids.ID) (map[ids.NodeID]*GetValidatorOutput, error)
+// State is an alias for validators.State for backward compatibility.
+type State = validators.State
 
-	// GetCurrentValidators returns current validators
-	GetCurrentValidators(ctx context.Context, height uint64, netID ids.ID) (map[ids.NodeID]*GetValidatorOutput, error)
+// GetValidatorOutput is an alias for validators.GetValidatorOutput
+type GetValidatorOutput = validators.GetValidatorOutput
 
-	// GetCurrentHeight returns the current height
-	GetCurrentHeight(ctx context.Context) (uint64, error)
+// WarpValidator is an alias for validators.WarpValidator
+type WarpValidator = validators.WarpValidator
 
-	// GetMinimumHeight returns the minimum acceptable height
-	GetMinimumHeight(ctx context.Context) (uint64, error)
+// WarpSet is an alias for validators.WarpSet
+type WarpSet = validators.WarpSet
 
-	// GetChainID returns the chain ID for a given network ID
-	GetChainID(netID ids.ID) (ids.ID, error)
+// Set is an alias for validators.Set
+type Set = validators.Set
 
-	// GetNetworkID returns the network ID for a given chain ID
-	GetNetworkID(chainID ids.ID) (ids.ID, error)
+// Validator is an alias for validators.Validator
+type Validator = validators.Validator
 
-	// GetWarpValidatorSets returns Warp validator sets for the requested heights and netIDs.
-	// Returns a map of netID -> height -> WarpSet containing BLS-enabled validators.
-	GetWarpValidatorSets(ctx context.Context, heights []uint64, netIDs []ids.ID) (map[ids.ID]map[uint64]*WarpSet, error)
+// ValidatorImpl is an alias for validators.ValidatorImpl
+type ValidatorImpl = validators.ValidatorImpl
 
-	// GetWarpValidatorSet returns the Warp validator set for a specific height and netID.
-	// Returns a WarpSet containing validators with BLS public keys for Warp signing.
-	GetWarpValidatorSet(ctx context.Context, height uint64, netID ids.ID) (*WarpSet, error)
-}
+// Manager is an alias for validators.Manager
+type Manager = validators.Manager
 
-// GetValidatorOutput provides validator information
-type GetValidatorOutput struct {
-	NodeID         ids.NodeID
-	PublicKey      []byte // BLS public key (classical)
-	RingtailPubKey []byte // Ringtail public key (post-quantum)
-	Light          uint64
-	Weight         uint64 // Alias for Light for backward compatibility
-	TxID           ids.ID // Transaction ID that added this validator
-}
+// SetCallbackListener is an alias for validators.SetCallbackListener
+type SetCallbackListener = validators.SetCallbackListener
 
-// WarpValidator represents a Warp validator with BLS and Ringtail keys
-type WarpValidator struct {
-	NodeID         ids.NodeID
-	PublicKey      []byte // BLS public key for Warp signing (classical)
-	RingtailPubKey []byte // Ringtail public key (post-quantum)
-	Weight         uint64
-}
+// ManagerCallbackListener is an alias for validators.ManagerCallbackListener
+type ManagerCallbackListener = validators.ManagerCallbackListener
 
-// WarpSet represents a set of Warp validators at a specific height
-type WarpSet struct {
-	Height     uint64
-	Validators map[ids.NodeID]*WarpValidator
-}
-
-// Set represents a set of validators
-type Set interface {
-	Has(ids.NodeID) bool
-	Len() int
-	List() []Validator
-	Light() uint64
-	Sample(size int) ([]ids.NodeID, error)
-}
-
-// Validator represents a validator
-type Validator interface {
-	ID() ids.NodeID
-	Light() uint64
-}
-
-// ValidatorImpl is a concrete implementation of Validator
-type ValidatorImpl struct {
-	NodeID   ids.NodeID
-	LightVal uint64
-}
-
-// ID returns the node ID
-func (v *ValidatorImpl) ID() ids.NodeID {
-	return v.NodeID
-}
-
-// Light returns the validator light
-func (v *ValidatorImpl) Light() uint64 {
-	return v.LightVal
-}
-
-// Manager manages validator sets
-type Manager interface {
-	GetValidators(netID ids.ID) (Set, error)
-	GetValidator(netID ids.ID, nodeID ids.NodeID) (*GetValidatorOutput, bool)
-	GetLight(netID ids.ID, nodeID ids.NodeID) uint64
-	GetWeight(netID ids.ID, nodeID ids.NodeID) uint64 // Deprecated: use GetLight
-	TotalLight(netID ids.ID) (uint64, error)
-	TotalWeight(netID ids.ID) (uint64, error) // Deprecated: use TotalLight
-
-	// Mutable operations
-	AddStaker(netID ids.ID, nodeID ids.NodeID, publicKey []byte, txID ids.ID, light uint64) error
-	AddWeight(netID ids.ID, nodeID ids.NodeID, light uint64) error
-	RemoveWeight(netID ids.ID, nodeID ids.NodeID, light uint64) error
-	NumNets() int
-
-	// Additional utility methods
-	Count(netID ids.ID) int
-	NumValidators(netID ids.ID) int // Alias for Count
-	Sample(netID ids.ID, size int) ([]ids.NodeID, error)
-	GetValidatorIDs(netID ids.ID) []ids.NodeID
-	SubsetWeight(netID ids.ID, nodeIDs set.Set[ids.NodeID]) (uint64, error)
-	GetMap(netID ids.ID) map[ids.NodeID]*GetValidatorOutput
-	RegisterCallbackListener(listener ManagerCallbackListener)
-	RegisterSetCallbackListener(netID ids.ID, listener SetCallbackListener)
-}
-
-// SetCallbackListener listens to validator set changes
-type SetCallbackListener interface {
-	OnValidatorAdded(nodeID ids.NodeID, light uint64)
-	OnValidatorRemoved(nodeID ids.NodeID, light uint64)
-	OnValidatorLightChanged(nodeID ids.NodeID, oldLight, newLight uint64)
-}
-
-// ManagerCallbackListener listens to manager changes
-type ManagerCallbackListener interface {
-	OnValidatorAdded(netID ids.ID, nodeID ids.NodeID, light uint64)
-	OnValidatorRemoved(netID ids.ID, nodeID ids.NodeID, light uint64)
-	OnValidatorLightChanged(netID ids.ID, nodeID ids.NodeID, oldLight, newLight uint64)
-}
-
-// Connector handles validator connections
-type Connector interface {
-	Connected(ctx context.Context, nodeID ids.NodeID, nodeVersion *version.Application) error
-	Disconnected(ctx context.Context, nodeID ids.NodeID) error
-}
+// Connector is an alias for validators.Connector
+type Connector = validators.Connector
