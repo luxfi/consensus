1a6ad51015576a76ccad5c78a30b3a3c3b7f58cd
Zach Kelling <z@zeekay.io>
2026-01-26 08:04:48 -0800

    chore: update dependencies and refactor validator module

--- validator/uptime/state.go
+++ validator/uptime/state.go
@@ -1,22 +1,12 @@
 // Copyright (C) 2019-2025, Lux Industries Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
 
+// Package uptime re-exports github.com/luxfi/validators/uptime for backward compatibility.
 package uptime
 
 import (
-	"time"
-
-	"github.com/luxfi/ids"
+	"github.com/luxfi/validators/uptime"
 )
 
-// State tracks validator uptime
-type State interface {
-	// GetUptime returns the uptime for a validator
-	GetUptime(nodeID ids.NodeID, netID ids.ID) (time.Duration, time.Duration, error)
-
-	// SetUptime sets the uptime for a validator
-	SetUptime(nodeID ids.NodeID, netID ids.ID, uptime time.Duration, lastUpdated time.Time) error
-
-	// GetStartTime returns when the validator started
-	GetStartTime(nodeID ids.NodeID, netID ids.ID) (time.Time, error)
-}
+// State is an alias for uptime.State
+type State = uptime.State
